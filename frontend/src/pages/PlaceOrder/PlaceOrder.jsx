3509667e7384bddf799be8f82a5a67b1612b2c1c
Mohammad Shoaib <mohammad.shoaib.oneone@gmail.com>
2026-09-23 11:52:33 +0600

    food-app place-order is added

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,6 +1,55 @@
+import { useContext } from "react"
+import { StoreContext } from "../../context/StoreContext"
+import './PlaceOrder.css'
+
 const PlaceOrder = () => {
+
+    const { getTotalCartAmount } = useContext(StoreContext)
+
     return (
-        <div>PlaceOrder</div>
+        <form className="place-order">
+            <div className="place-order-left">
+                <p className="title">Delivery Information</p>
+                <div className="multi-fields">
+                    <input type="text" placeholder="First Name" />
+                    <input type="text" placeholder="Last Name" />
+                </div>
+
+                <input type="email" placeholder="Email address" />
+                <input type="text" placeholder="Street" />
+                <div className="multi-fields">
+                    <input type="text" placeholder="City" />
+                    <input type="text" placeholder="State" />
+                </div>
+                <div className="multi-fields">
+                    <input type="text" placeholder="Zip code" />
+                    <input type="text" placeholder="Country" />
+                </div>
+                <input type="tel" placeholder="Phone" />
+            </div>
+            <div className="place-order-right">
+                <div className="cart-total">
+                    <h2>Cart Totals</h2>
+                    <div>
+                        <div className="cart-total-details">
+                            <p>Subtotal</p>
+                            <p>${getTotalCartAmount()}</p>
+                        </div>
+                        <hr />
+                        <div className="cart-total-details">
+                            <p>Delivery Fee</p>
+                            <p>${2}</p>
+                        </div>
+                        <hr />
+                        <div className="cart-total-details">
+                            <p>Total</p>
+                            <p>${getTotalCartAmount() + 2}</p>
+                        </div>
+                    </div>
+                    <button>PROCEED TO PAYMENT</button>
+                </div>
+            </div>
+        </form>
     )
 }
 
