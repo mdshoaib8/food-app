fdc938b438897bf72b81988e1b6542b9c30cd955
Mohammad Shoaib <mohammad.shoaib.oneone@gmail.com>
2026-09-26 10:18:08 +0600

    food-app user-login and signup features are  added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js";
+import 'dotenv/config'
+import userRouter from "./routes/userRoute.js";
 
 // set dns for +srv protocol
 import dns from "dns"
@@ -27,6 +29,7 @@ connectDB();
 // api endpoints
 app.use("/api/food", foodRouter)
 app.use("/images", express.static('uploads'))
+app.use("/api/user", userRouter)
 
 app.get("/", (req, res) => {
     res.send("API is working...")
